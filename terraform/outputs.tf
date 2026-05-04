976a0bb501a08158616dd8293732afe5039d463f
KovyD20 <kovy.d20@gmail.com~>
2026-05-04 14:05:30 +0200

    fix: add namespace to k8s manifests, fix LB hostname output via data source

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,6 +6,14 @@ output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
 
+data "kubernetes_service" "hello_world" {
+  metadata {
+    name      = "hello-world"
+    namespace = "default"
+  }
+  depends_on = [kubernetes_manifest.service]
+}
+
 output "load_balancer_hostname" {
-  value = kubernetes_manifest.service.object.status.loadBalancer.ingress[0].hostname
+  value = data.kubernetes_service.hello_world.status.0.load_balancer.0.ingress.0.hostname
 }
